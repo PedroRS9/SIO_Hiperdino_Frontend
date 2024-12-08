684d59b0e4c123db7e00e99730bc5799ec0cfb02
Nahuel Cosme <ncdv.2003@gmail.com>
2024-12-08 17:30:58 +0000

    Order data loaded

--- src/app/shared/components/dashboard/view3/view3.component.ts
+++ src/app/shared/components/dashboard/view3/view3.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-view3',
-  imports: [],
+  imports: [HttpClientModule, NgForOf],
   templateUrl: './view3.component.html',
+  standalone: true,
   styleUrl: './view3.component.css'
 })
-export class View3Component {
+export class View3Component implements OnInit{
+  public filteredOrders: any[] = []; // Datos filtrados
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    const url: string = 'http://localhost:8080/orders'; // Cambia la URL si es necesario
+    this.http.get<any[]>(url).subscribe(
+      (response) => {
+        this.filteredOrders = response; // Inicialmente, no hay filtros
+        console.log(this.filteredOrders);
+      },
+      (error) => {
+        console.error('Error al cargar los datos:', error);
+      }
+    );
+  }
 }
